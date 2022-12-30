5b4b467a2916817af5cf609a8668ddc74b8e7970
abd bansky <abdbansky@gmail.com>
2022-12-30 02:08:30 +0100

    ajout de projet

--- portfolio-react/src/components/Projets.jsx
+++ portfolio-react/src/components/Projets.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import WorkImg from "../assets/projects/workImg.jpeg"
-import realEstate from "../assets/projects/realestate.jpg"
+import kasa from "../assets/projects/kasa.png"
+import kanap from "../assets/projects/kanap.png"
 
 
 const Projets = () => {
@@ -18,7 +18,7 @@ const Projets = () => {
 
                     {/*grid items*/}
                     <div
-                        style={{ backgroundImage: `url(${WorkImg})` }}
+                        style={{ backgroundImage: `url(${kasa})` }}
                         className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
 
 
@@ -29,10 +29,10 @@ const Projets = () => {
 
                             </span>
                             <div className="pt-8 text-center">
-                                <a href="/">
+                                <a href="https://goanex32.github.io/P7kasa/">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" >Demo</button>
                                 </a>
-                                <a href="/">
+                                <a href="https://github.com/GOANEX32/P7kasa">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                                 </a>
                             </div>
@@ -41,21 +41,21 @@ const Projets = () => {
                     </div>
 
                     {/*grid items*/}
-                    <div style={{ backgroundImage: `url(${realEstate})` }}
+                    <div style={{ backgroundImage: `url(${kanap})` }}
                         className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
 
 
                         {/* Hover Effects*/}
                         <div className="opacity-0 group-hover:opacity-100">
                             <span className="text-2xl font-bold text-white tracking-wider">
-                                React JS Application
+                            javascript Application
 
                             </span>
                             <div className="pt-8 text-center">
-                                <a href="/">
+                                <a href="https://goanex32.github.io/KanapP5/">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" >Demo</button>
                                 </a>
-                                <a href="/">
+                                <a href="https://github.com/GOANEX32/KanapP5a">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                                 </a>
                             </div>
@@ -66,7 +66,7 @@ const Projets = () => {
 
 
                     {/*grid items*/}
-                    <div style={{ backgroundImage: `url(${realEstate})` }}
+                    <div style={{ backgroundImage: `url(${kanap})` }}
                         className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
 
 
