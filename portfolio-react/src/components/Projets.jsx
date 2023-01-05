b3c939ed5f059a1eed2c269e83439481aecd04b8
abd bansky <abdbansky@gmail.com>
2023-01-05 19:42:19 +0100

    kanap ajout

--- portfolio-react/src/components/Projets.jsx
+++ portfolio-react/src/components/Projets.jsx
@@ -55,7 +55,7 @@ const Projets = () => {
                                 <a href="https://goanex32.github.io/KanapP5/">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg" >Demo</button>
                                 </a>
-                                <a href="https://github.com/GOANEX32/KanapP5a">
+                                <a href="https://github.com/GOANEX32/KanapP5">
                                     <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Code</button>
                                 </a>
                             </div>
